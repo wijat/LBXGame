fdbb54964293112f559f340adc14edaf1e851fc7
徐辉杰 <82544268@qq.com>
2016-06-30 15:48:24 +0800

    更新版本

--- src/settings.js
+++ src/settings.js
@@ -10,62 +10,183 @@ _CCSettings = {
     ],
     "rawAssets": {
         "assets": {
-            "80c4565e-33d2-4cfb-b716-8130866c2143": [
-                "starScene.fire",
+            "c8967c94-2e53-4d13-8619-d3d6a045d5b7": [
+                "scene/loadResScene.fire",
                 "cc.SceneAsset"
             ],
             "2b86f30c-0a7f-42b2-8fe9-932edd53521e": [
-                "gameScene.fire",
+                "scene/gameScene.fire",
                 "cc.SceneAsset"
             ],
+            "80c4565e-33d2-4cfb-b716-8130866c2143": [
+                "scene/startScene.fire",
+                "cc.SceneAsset"
+            ],
+            "647d0a20-b0f1-4787-9982-8645c013696a": [
+                "resources/pics/bg.jpg",
+                "cc.Texture2D"
+            ],
             "65f721de-aa62-41fe-9e77-a7dc6d47155e": [
-                "pics/bg",
+                "resources/pics/bg",
                 "cc.SpriteFrame",
                 1
             ],
-            "406af525-60e1-4494-980a-099aa2ec82b0": [
-                "pics/pics",
+            "e1733586-b493-4e7f-802b-a50c88d4ccf1": [
+                "resources/pics/kuang.png",
+                "cc.Texture2D"
+            ],
+            "6838a73b-6945-491c-988b-e2fc48a2712a": [
+                "resources/pics/kuang",
                 "cc.SpriteFrame",
                 1
             ],
             "790fb661-4b52-4958-86fb-a373b8ae223c": [
-                "pics/pics.plist",
+                "resources/pics/pics.plist",
                 "cc.SpriteAtlas"
             ],
-            "022c1158-dd90-4a80-90ff-6be8edbcc702": [
-                "pics/pics",
-                "cc.SpriteFrame",
-                1
-            ],
             "b8d957f9-37b7-4101-a6b6-4c6b94115dfa": [
-                "pics/pics",
+                "resources/pics/pics",
                 "cc.SpriteFrame",
                 1
             ],
-            "91511ac2-e9c1-41c1-96e0-a2a7582d93d6": [
-                "anim/bianda.anim",
-                "cc.AnimationClip"
+            "406af525-60e1-4494-980a-099aa2ec82b0": [
+                "resources/pics/pics",
+                "cc.SpriteFrame",
+                1
             ],
-            "8aa9941d-f323-4096-9b54-bcb47c3d5b60": [
-                "sound/an.mp3",
-                "cc.AudioClip"
+            "1e1a1f89-1ba3-4d5b-a99f-69172239b8e2": [
+                "resources/pics/pics",
+                "cc.SpriteFrame",
+                1
             ],
-            "0fc7dc08-4e1e-471e-bb5c-009366c07c07": [
-                "sound/bgm.mp3",
-                "cc.AudioClip"
+            "d5c4b64f-eaf7-4e86-87c2-73e9e2f2bbbe": [
+                "resources/pics/pics",
+                "cc.SpriteFrame",
+                1
+            ],
+            "cfa2ac09-645c-47f2-8d59-967c729e69c7": [
+                "resources/pics/pics",
+                "cc.SpriteFrame",
+                1
+            ],
+            "d52ad9fa-b89c-4bf8-a6df-44472ad741e9": [
+                "resources/pics/pics",
+                "cc.SpriteFrame",
+                1
+            ],
+            "d900984a-7160-4831-9a56-ab1a22c4ed3f": [
+                "resources/pics/pics",
+                "cc.SpriteFrame",
+                1
+            ],
+            "3d8b512d-f080-4294-b5e8-3e2ab006b980": [
+                "resources/pics/pics",
+                "cc.SpriteFrame",
+                1
             ],
             "46d7a23b-9b74-48a1-9891-0356515e54a7": [
-                "pics/pics",
+                "resources/pics/pics",
                 "cc.SpriteFrame",
                 1
             ],
             "be8726cd-4146-41a8-9320-cf5d4dfe0140": [
-                "pics/pics",
+                "resources/pics/pics",
                 "cc.SpriteFrame",
                 1
             ],
+            "0564cc2f-e4b3-4e38-825f-b8bf10428e3d": [
+                "resources/pics/pics",
+                "cc.SpriteFrame",
+                1
+            ],
+            "6a1b18c9-6f69-421a-a0fb-e632501b998d": [
+                "resources/pics/pics",
+                "cc.SpriteFrame",
+                1
+            ],
+            "072b3db6-80f0-4cb5-a9d3-77edc26addc1": [
+                "resources/pics/pics",
+                "cc.SpriteFrame",
+                1
+            ],
+            "6dd9d1f7-a1ee-445c-87a0-4835f1bcd7d2": [
+                "resources/pics/pics",
+                "cc.SpriteFrame",
+                1
+            ],
+            "022c1158-dd90-4a80-90ff-6be8edbcc702": [
+                "resources/pics/pics",
+                "cc.SpriteFrame",
+                1
+            ],
+            "5f22aa18-49ae-499a-8b3d-e5b9dda46438": [
+                "resources/pics/pics",
+                "cc.SpriteFrame",
+                1
+            ],
+            "a05e7b9d-de71-4b0e-b10a-9dcab8e753a3": [
+                "resources/pics/pics",
+                "cc.SpriteFrame",
+                1
+            ],
+            "a6affece-5e13-4d19-b3dc-ee7f8467c12b": [
+                "resources/pics/pics",
+                "cc.SpriteFrame",
+                1
+            ],
+            "1b82c19c-d8c4-4e35-a9ed-d84ae09d46f0": [
+                "resources/pics/pics",
+                "cc.SpriteFrame",
+                1
+            ],
+            "a91361b4-7294-4a99-8c66-113c3dc9d65e": [
+                "resources/pics/pics",
+                "cc.SpriteFrame",
+                1
+            ],
+            "0e5c8e40-9880-4641-9f98-e985bf9d41ce": [
+                "resources/pics/pics.png",
+                "cc.Texture2D"
+            ],
+            "469ac758-8727-44cc-839a-f1c1da88ba0c": [
+                "resources/pics/pics",
+                "cc.SpriteFrame",
+                1
+            ],
+            "8aa9941d-f323-4096-9b54-bcb47c3d5b60": [
+                "resources/sound/an.mp3",
+                "cc.AudioClip"
+            ],
+            "5e503bc7-20fd-4dd0-a7f4-c2e2b10a97d8": [
+                "resources/sound/an1.mp3",
+                "cc.AudioClip"
+            ],
+            "0fc7dc08-4e1e-471e-bb5c-009366c07c07": [
+                "resources/sound/bgm.mp3",
+                "cc.AudioClip"
+            ],
+            "d6263b85-f46f-4594-b718-2fd1202a710f": [
+                "resources/sound/cannot1.mp3",
+                "cc.AudioClip"
+            ],
+            "7c27a4c6-0b69-47b4-ba07-dfe11d820b4e": [
+                "resources/sound/cannot2.mp3",
+                "cc.AudioClip"
+            ],
+            "1dacb76e-3ebb-44ad-9fd0-7fa0a3ad7e23": [
+                "resources/sound/fangxia1.mp3",
+                "cc.AudioClip"
+            ],
+            "08225cbd-7055-439c-9100-33206ac35e71": [
+                "resources/sound/fangxia2.mp3",
+                "cc.AudioClip"
+            ],
+            "c0cfcf7f-3a85-4be4-8ae4-7ca83fa6785e": [
+                "resources/sound/fangxia3.mp3",
+                "cc.AudioClip"
+            ],
             "cf83bd1c-b505-4e10-8892-6439df23d3af": [
-                "sound/xiaochu.mp3",
+                "resources/sound/xiaochu.mp3",
                 "cc.AudioClip"
             ],
             "4fd10055-ccca-426c-af70-fc5a5c65d157": [
@@ -76,120 +197,21 @@ _CCSettings = {
                 "prefab/boomEff.prefab",
                 "cc.Prefab"
             ],
-            "072b3db6-80f0-4cb5-a9d3-77edc26addc1": [
-                "pics/pics",
-                "cc.SpriteFrame",
-                1
-            ],
-            "5f22aa18-49ae-499a-8b3d-e5b9dda46438": [
-                "pics/pics",
-                "cc.SpriteFrame",
-                1
-            ],
-            "a05e7b9d-de71-4b0e-b10a-9dcab8e753a3": [
-                "pics/pics",
-                "cc.SpriteFrame",
-                1
-            ],
-            "a6affece-5e13-4d19-b3dc-ee7f8467c12b": [
-                "pics/pics",
-                "cc.SpriteFrame",
-                1
-            ],
-            "1b82c19c-d8c4-4e35-a9ed-d84ae09d46f0": [
-                "pics/pics",
-                "cc.SpriteFrame",
-                1
-            ],
-            "a91361b4-7294-4a99-8c66-113c3dc9d65e": [
-                "pics/pics",
-                "cc.SpriteFrame",
-                1
-            ],
-            "1dacb76e-3ebb-44ad-9fd0-7fa0a3ad7e23": [
-                "sound/fangxia1.mp3",
-                "cc.AudioClip"
-            ],
-            "08225cbd-7055-439c-9100-33206ac35e71": [
-                "sound/fangxia2.mp3",
-                "cc.AudioClip"
-            ],
-            "c0cfcf7f-3a85-4be4-8ae4-7ca83fa6785e": [
-                "sound/fangxia3.mp3",
-                "cc.AudioClip"
-            ],
-            "d6263b85-f46f-4594-b718-2fd1202a710f": [
-                "sound/cannot1.mp3",
-                "cc.AudioClip"
-            ],
-            "7c27a4c6-0b69-47b4-ba07-dfe11d820b4e": [
-                "sound/cannot2.mp3",
-                "cc.AudioClip"
+            "c394b75e-a079-49aa-8fde-616758790d9a": [
+                "prefab/tip.prefab",
+                "cc.Prefab"
             ],
             "a4756760-f98b-4e09-b3c5-12002cc39028": [
                 "fonts/mikado_outline_shadow.fnt",
                 "cc.BitmapFont"
             ],
-            "d900984a-7160-4831-9a56-ab1a22c4ed3f": [
-                "pics/pics",
-                "cc.SpriteFrame",
-                1
-            ],
-            "6dd9d1f7-a1ee-445c-87a0-4835f1bcd7d2": [
-                "pics/pics",
-                "cc.SpriteFrame",
-                1
-            ],
-            "3d8b512d-f080-4294-b5e8-3e2ab006b980": [
-                "pics/pics",
-                "cc.SpriteFrame",
-                1
-            ],
-            "0e5c8e40-9880-4641-9f98-e985bf9d41ce": [
-                "pics/pics.png",
-                "cc.Texture2D"
-            ],
-            "1e1a1f89-1ba3-4d5b-a99f-69172239b8e2": [
-                "pics/pics",
-                "cc.SpriteFrame",
-                1
-            ],
-            "d5c4b64f-eaf7-4e86-87c2-73e9e2f2bbbe": [
-                "pics/pics",
-                "cc.SpriteFrame",
-                1
-            ],
-            "cfa2ac09-645c-47f2-8d59-967c729e69c7": [
-                "pics/pics",
-                "cc.SpriteFrame",
-                1
-            ],
-            "d52ad9fa-b89c-4bf8-a6df-44472ad741e9": [
-                "pics/pics",
-                "cc.SpriteFrame",
-                1
-            ],
-            "0564cc2f-e4b3-4e38-825f-b8bf10428e3d": [
-                "pics/pics",
-                "cc.SpriteFrame",
-                1
-            ],
-            "6a1b18c9-6f69-421a-a0fb-e632501b998d": [
-                "pics/pics",
-                "cc.SpriteFrame",
-                1
-            ],
-            "647d0a20-b0f1-4787-9982-8645c013696a": [
-                "pics/bg.jpg",
-                "cc.Texture2D"
+            "91511ac2-e9c1-41c1-96e0-a2a7582d93d6": [
+                "anim/bianda.anim",
+                "cc.AnimationClip"
             ],
             "590e322d-76d6-472c-a90f-54c0b1103e28": [
                 "anim/zhaxia.anim",
                 "cc.AnimationClip"
-            ],
-            "e1733586-b493-4e7f-802b-a50c88d4ccf1": [
-                "pics/kuang.png",
-                "cc.Texture2D"
             ],
             "1693e519-47b5-4b4b-9562-25faab97e0e5": [
                 "fonts/mikado_outline_shadow.png",
@@ -207,25 +229,29 @@ _CCSettings = {
                 "cc.SpriteFrame",
                 1
             ],
-            "71561142-4c83-4933-afca-cb7a17f67053": [
-                "image/default_btn_disabled.png",
-                "cc.Texture2D"
-            ],
             "b43ff3c2-02bb-4874-81f7-f2dea6970f18": [
                 "image/default_btn_pressed.png",
+                "cc.Texture2D"
+            ],
+            "71561142-4c83-4933-afca-cb7a17f67053": [
+                "image/default_btn_disabled.png",
                 "cc.Texture2D"
             ]
         }
     },
-    "launchScene": "db://assets/starScene.fire",
+    "launchScene": "db://assets/scene/loadResScene.fire",
     "scenes": [
         {
-            "url": "db://assets/starScene.fire",
-            "uuid": "80c4565e-33d2-4cfb-b716-8130866c2143"
+            "url": "db://assets/scene/loadResScene.fire",
+            "uuid": "c8967c94-2e53-4d13-8619-d3d6a045d5b7"
         },
         {
-            "url": "db://assets/gameScene.fire",
+            "url": "db://assets/scene/gameScene.fire",
             "uuid": "2b86f30c-0a7f-42b2-8fe9-932edd53521e"
+        },
+        {
+            "url": "db://assets/scene/startScene.fire",
+            "uuid": "80c4565e-33d2-4cfb-b716-8130866c2143"
         }
     ],
     "orientation": "portrait",
